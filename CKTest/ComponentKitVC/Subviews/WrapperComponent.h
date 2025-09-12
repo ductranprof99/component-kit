d8d83c3a97cdbc4799201e02fc195f779a08c2f8
Duc Tran <ductd@MacBook-Pro-cua-ductd.local>
2025-09-12 16:59:03 +0700

    Add files

---
+++ CKTest/ComponentKitVC/Subviews/WrapperComponent.h
@@ -0,0 +1,17 @@
+//
+//  WrapperComponent.h
+//  CKTest
+//
+//  Created by ductd on 12/9/25.
+//
+
+#import <ComponentKit/ComponentKit.h>
+
+
+@class CellModel
+
+@interface WrapperCompomemt : CKCompositeComponent
+
++
+
+@end
